3202bd2c2a1e17369fd998998f87e09d7193cc97
kubersharma001 <kubersharma001@gmail.com>
2020-10-12 12:17:15 +0200

    dodge: include QCOM sepolicy
    Change-Id: I96714846258d6c409ed9257da15ac2281bfd1d9b

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -118,6 +118,9 @@ TARGET_USERIMAGES_USE_F2FS := true
 # Security patch level
 VENDOR_SECURITY_PATCH := 2025-01-01
 
+# SEPolicy
+include device/qcom/sepolicy/SEPolicy.mk
+
 # Verified Boot
 BOARD_AVB_ENABLE := true
 BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --flags 3
