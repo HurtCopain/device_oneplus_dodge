b0fa738f8ba8356b412bc3a70f79db2cc7501066
Succoria <tusk5051@apsinbox.com>
2025-04-11 14:29:08 -0700

    dodge: Start adapting for OP13

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -5,18 +5,18 @@
 #
 
 # Include the common OEM chipset BoardConfig.
-include device/oneplus/sm8650-common/BoardConfigCommon.mk
+include device/oneplus/sm8750-common/BoardConfigCommon.mk
 
-DEVICE_PATH := device/oneplus/waffle
+DEVICE_PATH := device/oneplus/dodge
 
 # Assert
-TARGET_OTA_ASSERT_DEVICE := OP5929L1,OP595DL1
+TARGET_OTA_ASSERT_DEVICE := OP5D55L1,OP5D55L1
 
 # Display
 TARGET_SCREEN_DENSITY := 640
 
 # Kernel
-TARGET_KERNEL_CONFIG += vendor/oplus/waffle.config
+TARGET_KERNEL_CONFIG += vendor/oplus/dodge.config
 
 # Properties
 TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor.prop
@@ -25,4 +25,4 @@ TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor.prop
 TARGET_RECOVERY_UI_MARGIN_HEIGHT := 103
 
 # Include the proprietary files BoardConfig.
-include vendor/oneplus/waffle/BoardConfigVendor.mk
+include vendor/oneplus/dodge/BoardConfigVendor.mk
--- lineage_dodge.mk
+++ lineage_dodge.mk
@@ -8,24 +8,24 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit_only.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit from waffle device
-$(call inherit-product, device/oneplus/waffle/device.mk)
+# Inherit from dodge device
+$(call inherit-product, device/oneplus/dodge/device.mk)
 
 # Inherit some common Lineage stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
-PRODUCT_NAME := lineage_waffle
-PRODUCT_DEVICE := waffle
+PRODUCT_NAME := lineage_dodge
+PRODUCT_DEVICE := dodge
 PRODUCT_MANUFACTURER := OnePlus
 PRODUCT_BRAND := OnePlus
-PRODUCT_MODEL := CPH2581
+PRODUCT_MODEL := CPH2655
 
 PRODUCT_GMS_CLIENTID_BASE := android-oneplus
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    BuildDesc="CPH2581EEA-user 15 AP3A.240617.008 U.R4T3.1c799fe_ca44_2 release-keys" \
-    BuildFingerprint=OnePlus/CPH2581EEA/OP595DL1:15/AP3A.240617.008/U.R4T3.1c799fe_ca44_2:user/release-keys \
-    DeviceName=OP595DL1 \
-    DeviceProduct=CPH2581 \
-    SystemDevice=OP595DL1 \
-    SystemName=CPH2581
+    BuildDesc="qssi_64-user 15 AP3A.240617.008 1742534230950 release-keys" \
+    BuildFingerprint=OnePlus/CPH2655/OP5D55L1:15/AP3A.240617.008/V.R4T3.1d186e6-ee3a-ee3b:user/release-keys \
+    DeviceName=OP5D55L1 \
+    DeviceProduct=CPH2655 \
+    SystemDevice=OP5D55L1 \
+    SystemName=CPH2655
